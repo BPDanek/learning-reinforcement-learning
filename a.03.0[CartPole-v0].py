d80b7fc3c160c94c3e62a71b849b35e555b9ec65
denbanek <danekbenjamin@gmail.com>
2018-12-21 23:47:36 +0100

    First addition of QTable, wanted to have a clean slate before anything goes down

--- a.03.0[CartPole-v0].py
+++ a.03.0[CartPole-v0].py
@@ -1,30 +1,34 @@
-# learning how to work out at the gym: a.02.2[CartPole-v0]
+# learning how to work out at the gym: a.03.0[CartPole-v0]
+# Q-Table attempt 1
 
-# make experiment have modular action
-# clean up workspace so
-#       mental organization
-#       reusability
-
-# organization:
-# produce environment (env) of cartpole
+# organization: (from previous)
+# produce environment (env) of CartPole
 # produce an experiment using globals and the passed param: Policy Signal
 #
 # in each experiment we run the number of episodes, each episode with the policy signal known
 #       in the episode, a multiplexer is called with the signal as parameter, which returns an action
 #           the purpose of the signal is to determine what action to take (decision policy)
 
-import gym # cartpole-v0
+import gym  # CartPole-v0 dependancy
 
 # some constants
-NUM_EPISODES = 12
+NUM_EPISODES = 2
 MAX_REWARD = 100
-RANDOM_SELECT = 0
-ALGORITHMIC_SELECT = 1
+
+RANDOM_SELECT =         0
+ALGORITHMIC_SELECT =    1
+QTABLE_SELECT =         2
 
 # use single global env that gets reset per episode
 env = gym.make('CartPole-v0')
 
-# return policy given observation object; policy is designed here
+
+# BASELINE: sample random (rnd) policy from space
+def rnd_policy(observation):
+    return env.action_space.sample()
+
+
+# BASELINE: return policy given observation object; policy is designed here
 def alg_policy(observation):
     # policy: if pole leaning left, move left (put center of gravity below tip)
     #         if pole leaning right, move right (angle positive (observation 2), move right (action 1))
@@ -34,17 +38,26 @@ def alg_policy(observation):
         return 0
 
 
-# sample random (rnd) policy from space
-def rnd_policy(observation):
-    return env.action_space.sample()
+# Q-TABLE: implement a means of producing some learned policy in the program
+#   each possible state, action pair will have a determined (quality) Q-value
+#   with a complete table of Q values, we can safely be put into any state and know what the best next move is
+#   with that being said, the difficulty is learning how to determine these Q-values
+#   TODO: How are Q values learned?
+def qtable_policy(observation):
+    # TODO: implementation
 
 
 # returns action decided by signal, 0 for random, 1 for algorithmic
 def action_mux(policy_signal, observation):
-    if policy_signal == 0:
-        return rnd_policy(observation)
-    else: # signal = 1
+
+    if policy_signal == ALGORITHMIC_SELECT:
         return alg_policy(observation)
+
+    elif policy_signal == QTABLE_SELECT:
+        return qtable_policy(observation)
+
+    else: # signal = 0 (RANDOM_SELECT)
+        return rnd_policy(observation)
 
 
 def experiment(policy_signal):
@@ -69,6 +82,8 @@ def experiment(policy_signal):
 
             total_reward = total_reward + t_reward
 
+
+            # debugging info:
             # print('a_:', action)
             # print('o2:', observation[2])  # object, 4 values shown below
             # print('d_:', done)
@@ -93,5 +108,9 @@ print('random policy: ')
 experiment(RANDOM_SELECT)
 print('algorithmic policy:')
 experiment(ALGORITHMIC_SELECT)
+# print('algorithmic policy:')
+# [alg_policy]
 
+
+# saves pyglet some worry
 env.close()
