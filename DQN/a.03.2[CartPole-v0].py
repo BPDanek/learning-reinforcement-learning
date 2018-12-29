abcc8194ce8f34a798126b514bca2a12601e44de
denbanek <danekbenjamin@gmail.com>
2018-12-29 14:23:19 +0100

    First addition of QTable, wanted to have a clean slate before anything goes down

--- DQN/a.03.2[CartPole-v0].py
+++ DQN/a.03.2[CartPole-v0].py
@@ -23,7 +23,7 @@
 import numpy as np
 import gym
 import torch.nn as nn
-#import DQNetwork
+from DQN import DQNetwork as Q
 
 
 NUMBER_OF_MEMORIES = 100 # number of "memories," states that are available in action replay
@@ -33,18 +33,13 @@ SIZE_OF_MEMORY_CONTENTS = 4 # (state, action, reward, new_state)
 # initialize memory replay with size N (NUMBER_OF_MEMORIES)
 replay_memory = np.zeros((NUMBER_OF_MEMORIES, SIZE_OF_MEMORY_CONTENTS))
 
-#dqn = DQN()
-
-env = gym.make('CartPole-v0')
+net = Q.DQNetwork()  # network defined in DQNetwork file
+env = gym.make('CartPole-v0')  # CartPole simulation environment from gym
 
 # reset() generates a single observation, which is (4,) with each element a uniform value between [-0.05, 0.05]
 observation = env.reset()
-print(observation)
 
-model = nn.Sequential(
-          nn.Conv2d(1,20,5),
-          nn.ReLU(),
-          nn.Conv2d(20,64,5),
-          nn.ReLU()
-        )
-print(model)
+print(net)
+
+
+
