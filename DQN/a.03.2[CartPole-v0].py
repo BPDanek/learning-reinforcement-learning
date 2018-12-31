f3cb497044690b7c5ffd8dc284f23de79c6cc530
denbanek <danekbenjamin@gmail.com>
2018-12-31 19:11:10 +0100

    non-inference mode functional

--- DQN/a.03.2[CartPole-v0].py
+++ DQN/a.03.2[CartPole-v0].py
@@ -27,9 +27,11 @@
 
 
 # things to change:
-# explore/exploit decay rate should probably have some scientific backing
+# explore/exploit decay rate should probably have some scientific backing (i feel like it should be more gradual)
 # neural network architecture (layers, number of nodes, activations)
-# imporove means of plotting pyplot stuff
+# imporove means of plotting pyplot stuff (excessive variables right now, really complicated)
+# explain the "exploration limit" better
+# explore action function paired to exploit action function
 
 # IMPORTS ---------------------------------------------------------------------------------------------------------
 
@@ -47,7 +49,7 @@ MEMORIES_PER_TRAINING = 3
 # number of episodes over which to perform learning of Q estimator
 EPISODES = 1
 
-NUMBER_OF_MEMORIES = 100 # number (volume) of "memories," states that are available in action replay
+NUMBER_OF_MEMORIES = 1000 # number (volume) of "memories," states that are available in action replay
 
 SIZE_OF_MEMORY_CONTENTS = 4 # number of entries per memory in memory replay: (state, action, reward, new_state)
 
@@ -101,23 +103,30 @@ class DQNetwork(nn.Module):
 
 # Algorithm Implementation ----------------------------------------------------------------------------------------
 
-dummy = (2, 2, 2, 2)
 
 # initialize memory replay with size N (NUMBER_OF_MEMORIES)
 replay_memory = np.zeros((NUMBER_OF_MEMORIES, SIZE_OF_MEMORY_CONTENTS))
 memory_index = 0 # counts index of replay memory
 
-print(replay_memory[0])
-replay_memory[0] = dummy
-print(replay_memory[0])
-
-net = DQNetwork()
 env = gym.make('CartPole-v0')  # CartPole simulation environment from gym
-
-
 # reset() generates a single observation, which is (4,) with each element a uniform value between [-0.05, 0.05]
 
+policy_net = DQNetwork()
+target_net = DQNetwork()
+
+
+
+
+# return argmax (action) given state from Neural Network
+# neural network usage derived roughly from:
+# https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html#dqn-algorithm
+def exploit_action():
+
+
+
+
 def run_training_operation(verbose=False, show_plot=False):
+
     for episode in range(EPISODES):
 
         # used in print statements and debugging, should be incremented within while loop
@@ -130,10 +139,10 @@ def run_training_operation(verbose=False, show_plot=False):
 
         # some parameters:
         # exploration-exploitation cutoffs, epsilon below explore, explore. Epsilon above explore, exploit
-        explore_var_start = 0
+        explore_var_start = 0  # a value that will be iteratively updated, and used to derive our exploration limit
         explore_min = 0.01
-        explore_decay = 0.01 # explore value will decay within the while loop at this fixed rate
-        decay_step = 0 # a component of the function that adjusts the bar
+        explore_decay = 0.01  # explore value will decay within the while loop at this fixed rate
+        explore_decay_step = 0  # a component of the function that adjusts the bar used to determine this tradeoff
 
         # resetting environment returns initial observation
         observation = env.reset()
@@ -152,40 +161,56 @@ def run_training_operation(verbose=False, show_plot=False):
         #   action determined by exploration-exploitation tradeoff
         #   learning done by NN instance, sample to learn comes from memory bank
         while not done:
-            iteration += 1
-            iterator_plus += 1
 
-            # exploration-exploitation
-            epsilon = np.random.rand()
+            # update some iterators, as that's their purpose
+            iteration += 1; iterator_plus += 1
+
+            # exploration-exploitation tradeoff epsilon used to decide when to exploit and when to explore
+            epsilon_rand = np.random.rand()
 
             # exploration decline is defined as such (by myself, may need to be optimized)
             current_explore_limit = 1 - (explore_var_start ** 2)
 
-            # update for pyplot
-            print("index: {}".format(iteration))
+            # update a log of a changing variable for pyplot; this variable changes per iteration of while loop
             current_explore_limit_BUFFER.append(current_explore_limit)
 
             # increment before we update explore_var_start
-            decay_step += 1
+            explore_decay_step += 1
 
+            # calculating decay of exploration probability:
             # we don't want to decay below zero, always retain some small* chance of
             # chose (3/8) because this roughly translates to "below 13%" (normal dist)
             if not current_explore_limit < (3/8):
                 # need to update explore_start rate so that it's a little bigger each time
-                explore_var_start = explore_var_start + explore_decay*decay_step
+                explore_var_start = explore_var_start + explore_decay*explore_decay_step
 
-            # update for pyplot
+            # update a log of a changing variable for pyplot; this variable changes per iteration of while loop
             explore_var_start_BUFFER.append(explore_var_start)
 
+            # TODO: remove this once building the code is done
+            if iteration == 20:
+                done = True
+
+
+            # select action to take
+            if current_explore_limit > epsilon_rand:
+                action = env.action_space.sample() # explore state space for new income
+            else:
+                action = exploit_action(observation) # exploit state space for best action
+
+
+
+
+            # ------ (start) some loud output statements ------
             # verbosity flag triggers some information about the current training loop
             if verbose is True:
 
                 if iteration == 0:  # start of while (unique print)
                     print("\n\n----Episode {} initiated----".format(episode))
 
-                print("Iteration {}\n\nCurrent values: ".format(iteration))
-                print("\tdecay_step: {}".format(decay_step))
-                print("\tepsilon: {:.4f}".format(epsilon))
+                print("\n\nIteration {}\n".format(iteration))
+                print("\texplore_decay_step: {}".format(explore_decay_step))
+                print("\tepsilon_rand: {:.4f}".format(epsilon_rand))
                 print("\texplore_var_start: {}".format(explore_var_start))
                 print("\tcurrent_explore_limit: {:.4f}".format(current_explore_limit))
 
@@ -193,10 +218,6 @@ def run_training_operation(verbose=False, show_plot=False):
                 print("\t\tvar: iteratr_plus range         {}".format((np.asarray(range(iterator_plus))).shape))
                 print("\t\tvar: explore_var_start size     {}".format((np.asarray(explore_var_start_BUFFER)).shape))
                 print("\t\tvar: current_explore_limit size {}".format((np.asarray(current_explore_limit_BUFFER)).shape))
-
-            if iteration == 45:
-                done = True
-
 
             # plotting explore_limit_var and explore_limit as a matplotlib.pyplot versus iterations to explore the
             # details of exploration-exploitation tradeoff
@@ -213,11 +234,15 @@ def run_training_operation(verbose=False, show_plot=False):
                 plt.ylabel("exploration variable and limit")
                 plt.legend()
                 plt.show()
+            # ------ (end) some loud output statements ------
+
+
+            # now that we've made probabilities for our explore/exploit tradeoff let's implement some actions
 
 
 
-
-print(net)
+#  print(range(env.action_space))
+#  print(net)
 run_training_operation(verbose=True, show_plot=True)
 
 
